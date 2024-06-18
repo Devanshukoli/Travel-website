d6e62e541d7abdaddca3b8008c2e0f87ded8ff7c
Devanshukoli <kolidevanshu02@gmail.com>
2024-06-18 18:18:42 +0530

    [feat] First Next Build.

---
+++ components/navbar.js
@@ -0,0 +1,136 @@
+import { forwardRef } from 'react';
+import Logo from './logo'
+import NextLink from 'next/link'
+import {
+  Container,
+  Box,
+  Link,
+  Stack,
+  Heading,
+  Flex,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuButton,
+  useColoreModeValue,
+  IconButton
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'; 
+import ThemeTogglebutton from './theme-toggle-button'
+import { IoLogoGithub } from 'react-icons/io5'
+
+const LinkItem = ({ href, path, target, children, ...props }) => {
+  const active = path === href
+  const inactiveColor = useColoreModeValue('gray.800', 'whiteAlpha.900')
+  return(
+  <Link
+    as={NextLink}
+    href={href}
+    scroll={false}
+    p={2}
+    bg={active ? 'grassTeal': undefined}
+    color={active ? '#202023': inactiveColor}
+    target={target}
+    {...props}
+    >
+      {children}
+    </Link>
+  )
+}
+
+const MenuLink = forwardRef((props, ref) => (
+  <Link ref={ref} as={NextLink} {...props} />
+))
+
+const Navbar = props = {
+  const {path} = props
+
+  return (
+    <Box
+    position="fixed"
+    as="nav"
+    w="100%"
+    bg={useColoreModeValue('#ffffff40', '#20202380')}
+    css={{ backdropFilter: 'blur(10px)'}}
+    zIndex={2}
+    {...props}
+  >
+    <Container
+      display='flex'
+      p={2}
+      maxW='container.md'
+      wrap="wrap"
+      align="center"
+      justify="space-between"
+    >
+      <Flex align='center' mr={5}>
+        <Heading as='h1' size='lg' letterSpacing={'tighter'}>
+          <Logo />
+        </Heading>
+      </Flex>
+
+      <Stack
+        direction={{ base: 'column', md: 'row' }}
+        display={{ base: 'none', md: 'flex' }}
+        width={{ base: 'full', md: 'auto' }}
+        alignItems='center'
+        flexGrow={1}
+        mt={{ base: 4, md: 0 }}
+      >
+        <LinkItem href='/works' path={path}>
+          Works
+        </LinkItem>
+        <LinkItem href='/wallpapers' path={path}>
+          Wallpapers
+        </LinkItem>
+        <LinkItem href='/posts' path={path}>
+          Posts
+        </LinkItem>
+        <LinkItem 
+          target='_blank'
+          href='https://github.com/Devanshukoli/Travel-website'
+          path={path}
+          display='inline-flex'
+          alignItems='center'
+          style={{ gape: 4 }}
+          pl={2}
+          >
+            <IoLogoGithub />
+            Source
+        </LinkItem>
+      </Stack>
+
+      <Box flex={1} align='right'>
+        <ThemeToggleButton />
+
+        <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+          <Menu isLazy id='navbar-menu'>
+            <MenuButton
+              as={IconButton}
+              icon={<HamburgerIcon />}
+              variant='outline'
+              aria-lable='Options'
+            />
+            <MenuList>
+              <MenuItem as={MenuLink} href='/about'>
+                About
+              </MenuItem>
+              <MenuItem as={MenuLink} href='/blog'>
+                Blog 
+              </MenuItem>
+              <MenuItem as={MenuLink} href='/contact'>
+                Contact
+              </MenuItem>
+              <MenuItem as={Link} href='https://github.com/Devanshukoli/Travel-website'>
+                View Source
+              </MenuItem>
+            </MenuList> 
+          </Menu>
+        </Box>
+      </Box>
+    </Container>
+    </Box>
+  )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,22 @@
 import NextLink from "next/link";
+import Layout from '../components/layouts/article'
+import { useColorModeValue, Container, Box } from "@chakra-ui/react";
+
+const Home = () => (
+  <Layout>
+    <Container>
+      <Box
+      borderRadius='lg'
+      mb={6}
+      p={3}
+      textAlign='center'
+      bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+      css={{ backdropFilter: 'blur(10px)' }}
+      >
+        Hello, I&apos;
+      </Box>
+    </Container>
+  </Layout>
+)
+
+export default Home
