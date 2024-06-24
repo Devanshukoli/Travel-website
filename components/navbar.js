c4364bc474bd04aea99bf84d61073ace5d25fb6b
Devanshukoli <kolidevanshu02@gmail.com>
2024-06-24 17:28:57 +0530

    [feat] Added navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,4 +1,3 @@
-import { forwardRef } from 'react';
 import Logo from './logo'
 import NextLink from 'next/link'
 import {
@@ -12,123 +11,82 @@ import {
   MenuItem,
   MenuList,
   MenuButton,
-  useColoreModeValue,
-  IconButton
+  IconButton,
+  useColorModeValue
 } from '@chakra-ui/react'
-import { HamburgerIcon } from '@chakra-ui/icons'; 
-import ThemeTogglebutton from './theme-toggle-button'
-import { IoLogoGithub } from 'react-icons/io5'
+import { HamburgerIcon } from '@chakra-ui/icons'
 
-const LinkItem = ({ href, path, target, children, ...props }) => {
+
+const LinkItem = ({ href, path, children }) => {
   const active = path === href
-  const inactiveColor = useColoreModeValue('gray.800', 'whiteAlpha.900')
-  return(
-  <Link
-    as={NextLink}
-    href={href}
-    scroll={false}
-    p={2}
-    bg={active ? 'grassTeal': undefined}
-    color={active ? '#202023': inactiveColor}
-    target={target}
-    {...props}
-    >
-      {children}
-    </Link>
+  const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
+  return (
+    <NextLink href={href}>
+      <Link
+        p={2}
+        bg={active ? 'glassTeal' : undefined}
+        color={active ? '#202023' : inactiveColor}
+      >
+        {children}
+      </Link>
+    </NextLink>
   )
 }
 
-const MenuLink = forwardRef((props, ref) => (
-  <Link ref={ref} as={NextLink} {...props} />
-))
-
-const Navbar = props = {
-  const {path} = props
+const Navbar = (props) => {
+  const { path } = props
 
   return (
     <Box
-    position="fixed"
-    as="nav"
-    w="100%"
-    bg={useColoreModeValue('#ffffff40', '#20202380')}
-    css={{ backdropFilter: 'blur(10px)'}}
-    zIndex={2}
-    {...props}
-  >
-    <Container
-      display='flex'
-      p={2}
-      maxW='container.md'
-      wrap="wrap"
-      align="center"
-      justify="space-between"
+      position="fixed"
+      as="nav"
+      w='100%'
+      bg={useColorModeValue('#ffffff40', '#20202380')}
+      style={{ backdropFilter: 'blur(10px)' }}
+      zIndex={1}
+      {...props}
     >
-      <Flex align='center' mr={5}>
-        <Heading as='h1' size='lg' letterSpacing={'tighter'}>
-          <Logo />
-        </Heading>
-      </Flex>
-
-      <Stack
-        direction={{ base: 'column', md: 'row' }}
-        display={{ base: 'none', md: 'flex' }}
-        width={{ base: 'full', md: 'auto' }}
-        alignItems='center'
-        flexGrow={1}
-        mt={{ base: 4, md: 0 }}
+      <Container
+        display="flex"
+        p={2}
+        maxW="container.md"
+        wrap="wrap"
+        align="center"
+        justify="space-between"
       >
-        <LinkItem href='/works' path={path}>
-          Works
-        </LinkItem>
-        <LinkItem href='/wallpapers' path={path}>
-          Wallpapers
-        </LinkItem>
-        <LinkItem href='/posts' path={path}>
-          Posts
-        </LinkItem>
-        <LinkItem 
-          target='_blank'
-          href='https://github.com/Devanshukoli/Travel-website'
-          path={path}
-          display='inline-flex'
+        <Flex align='center' mr={5}>
+          <Heading as='h1' size='lg' letterSpacing={'tighter'}>
+            <Logo />
+          </Heading>
+        </Flex>
+
+        <Stack
+          direction={{ base: 'column', md: 'row' }}
+          display={{ base: 'none', md: 'flex' }}
+          width={{ base: 'full', md: 'auto' }}
           alignItems='center'
-          style={{ gape: 4 }}
-          pl={2}
-          >
-            <IoLogoGithub />
-            Source
-        </LinkItem>
-      </Stack>
+          flexGrow={1}
+          mt={{ base: 4, nmd: 0 }}
+        >
+          <LinkItem href='/about' path={path}>
+            About
+          </LinkItem>
 
-      <Box flex={1} align='right'>
-        <ThemeToggleButton />
+          <LinkItem href='/contact' path={path}>
+            Contact Us
+          </LinkItem>
+        </Stack>
 
-        <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
-          <Menu isLazy id='navbar-menu'>
-            <MenuButton
-              as={IconButton}
-              icon={<HamburgerIcon />}
-              variant='outline'
-              aria-lable='Options'
-            />
-            <MenuList>
-              <MenuItem as={MenuLink} href='/about'>
-                About
-              </MenuItem>
-              <MenuItem as={MenuLink} href='/blog'>
-                Blog 
-              </MenuItem>
-              <MenuItem as={MenuLink} href='/contact'>
-                Contact
-              </MenuItem>
-              <MenuItem as={Link} href='https://github.com/Devanshukoli/Travel-website'>
-                View Source
-              </MenuItem>
-            </MenuList> 
-          </Menu>
+        <Box flex={1} align='right'>
+          <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+            <Menu>
+              <MenuButton as={IconButton} icon={<HamburgerIcon />} variant="outline" aria-label='Options' /> 
+
+            </Menu>
+          </Box>
         </Box>
-      </Box>
-    </Container>
+
+      </Container>
     </Box>
   )
 }
