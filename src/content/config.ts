61dff156fbc5b6eee2c4f712085cfefcc46beb76
Devanshukoli <kolidevanshu02@gmail.com>
2024-07-03 17:43:21 +0530

    refactor(defining-collections): :recycle: changed the export forms to official docs
    Well, Basiclly changed the export method in *content* folder's `config.ts` file into officil docs
    recommanded.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
+const blogCollection = defineCollection({
   type: "content",
   // Type-check frontmatter using a schema
   schema: z.object({
@@ -13,7 +13,7 @@ const blog = defineCollection({
   }),
 });
 
-const destination = defineCollection({
+const destinationCollection = defineCollection({
   type: "content",
   // Type-check frontmatter using a schema
   schema: z.object({
@@ -24,4 +24,7 @@ const destination = defineCollection({
   }),
 });
 
-export const collections = { blog, destination };
+export const collections = {
+  "blog": blogCollection,
+  "destination": destinationCollection
+};
